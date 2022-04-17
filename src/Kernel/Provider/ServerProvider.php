d1d16778d6e43ebcd3ca1a5e0da9c6f3634f8358
Aleksey Ishutin <ishutin.alexey@gmail.com>
2022-04-17 11:27:37 +0200

    new colors

--- src/Kernel/Provider/ServerProvider.php
+++ src/Kernel/Provider/ServerProvider.php
@@ -55,26 +55,26 @@ class ServerProvider implements ProviderInterface
         );
 
         foreach ($this->config->get('server.options', []) as $key => $option) {
-            echo \sprintf(" %s \e[1m\033[32m%s\033[0m" . \PHP_EOL, $key, $option);
+            echo \sprintf(" %s \e[1m\033[92m%s\033[0m" . \PHP_EOL, $key, $option);
         }
 
         echo "------------------------------\e[7mApplication settings\e[0m-------------------------------------------" . \PHP_EOL;
         echo sprintf(
             "* Debug mode: \e[1m%s\033[0m" . \PHP_EOL,
-            $this->config->get('app.debug', false) ? "\033[31mYes" : "\033[32mNo"
+            $this->config->get('app.debug', false) ? "\033[91mYes" : "\033[92mNo"
         );
-        echo sprintf("* Environments: \e[1m\033[32m%s\033[0m" . \PHP_EOL, $this->config->get('app.env', 'local'));
-        echo sprintf("* Language: \e[1m\033[32m%s\033[0m" . \PHP_EOL, $this->config->get('app.lng', 'en'));
-        echo sprintf("* Timezone: \e[1m\033[32m%s\033[0m" . \PHP_EOL, $this->config->get('app.timezone', 'UTC'));
+        echo sprintf("* Environments: \e[1m\033[92m%s\033[0m" . \PHP_EOL, $this->config->get('app.env', 'local'));
+        echo sprintf("* Language: \e[1m\033[92m%s\033[0m" . \PHP_EOL, $this->config->get('app.lng', 'en'));
+        echo sprintf("* Timezone: \e[1m\033[92m%s\033[0m" . \PHP_EOL, $this->config->get('app.timezone', 'UTC'));
         echo sprintf(
-            "* Loaded providers: \e[1m\033[32m%d\033[0m" . \PHP_EOL,
+            "* Loaded providers: \e[1m\033[92m%d\033[0m" . \PHP_EOL,
             count($this->config->get('app.providers', []))
         );
         foreach ($this->config->get('app.providers', []) as $provider) {
-            echo \sprintf(" - \e[1m\033[32m%s\033[0m" . \PHP_EOL, $provider);
+            echo \sprintf(" - \e[1m\033[92m%s\033[0m" . \PHP_EOL, $provider);
         }
         echo sprintf(
-            "* Loaded http actions: \e[1m\033[32m%d\033[0m" . \PHP_EOL,
+            "* Loaded http actions: \e[1m\033[92m%d\033[0m" . \PHP_EOL,
             count($this->config->get('http.actions', []))
         );
         echo "---------------------------------------------------------------------------------------------" . \PHP_EOL;
