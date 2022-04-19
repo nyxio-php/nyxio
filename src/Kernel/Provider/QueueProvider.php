0e502f9d3b929fbd95e8714e40a6a005764ed302
Aleksey Ishutin <ishutin.alexey@gmail.com>
2022-04-19 17:49:23 +0200

    update provider

--- src/Kernel/Provider/QueueProvider.php
+++ src/Kernel/Provider/QueueProvider.php
@@ -4,19 +4,26 @@ declare(strict_types=1);
 
 namespace Nyxio\Kernel\Provider;
 
+use Nyxio\Contract\Container\ContainerInterface;
 use Nyxio\Contract\Kernel\Server\ServerEventHandlerInterface;
 use Nyxio\Contract\Provider\ProviderInterface;
+use Nyxio\Contract\Queue\QueueInterface;
 use Nyxio\Kernel\Server\Queue\Handler\FinishEventHandler;
 use Nyxio\Kernel\Server\Queue\Handler\TaskEventHandler;
+use Nyxio\Kernel\Server\Queue\Queue;
 
 class QueueProvider implements ProviderInterface
 {
-    public function __construct(private readonly ServerEventHandlerInterface $eventHandler)
-    {
+    public function __construct(
+        private readonly ServerEventHandlerInterface $eventHandler,
+        private readonly ContainerInterface $container
+    ) {
     }
 
     public function process(): void
     {
+        $this->container->singleton(QueueInterface::class, Queue::class);
+
         $this->eventHandler->attach('task', TaskEventHandler::class);
         $this->eventHandler->attach('finish', FinishEventHandler::class);
     }
