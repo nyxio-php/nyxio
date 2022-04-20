0d162e5327390e33508a141f70b0fdff7f071261
Aleksey Ishutin <ishutin.alexey@gmail.com>
2022-04-20 19:39:29 +0200

    try to fix

--- src/Kernel/Server/Queue/Queue.php
+++ src/Kernel/Server/Queue/Queue.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nyxio\Kernel\Server\Queue;
 
+use Nyxio\Contract\Config\ConfigInterface;
 use Nyxio\Contract\Queue\OptionsInterface;
 use Nyxio\Contract\Queue\QueueInterface;
 use Nyxio\Kernel\Server\WorkerData;
@@ -11,8 +12,7 @@ use Swoole\Http\Server;
 
 class Queue implements QueueInterface
 {
-
-    public function __construct(private readonly Server $server)
+    public function __construct(private readonly Server $server, private readonly ConfigInterface $config)
     {
     }
 
@@ -22,12 +22,27 @@ class Queue implements QueueInterface
         ?OptionsInterface $options = null
     ): void {
         $workerData = new WorkerData($job, $data, $options);
-        $taskId = $this->server->task(data: $workerData);
-
-        if ($taskId === false) {
+        if (!$this->checkWorkersAvailability()) {
             $this->server->after(5000, function () use ($workerData) {
                 $this->server->task($workerData);
             });
+            return;
         }
+
+        $this->server->task($workerData);
+    }
+
+    public function checkWorkersAvailability(): bool
+    {
+        $workers = $this->config->get('server.options.worker_num')
+            * $this->config->get('server.options.task_worker_num');
+
+        for ($i = $workers - 1; $i >= 0; $i--) {
+            if ($this->server->getWorkerStatus($i) === \SWOOLE_WORKER_IDLE) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
