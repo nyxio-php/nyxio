83a30504001e2b9bc7cae6d4b4961d0b06ebcdb0
Aleksey Ishutin <ishutin.alexey@gmail.com>
2022-04-22 14:17:52 +0200

    new launcher for queue jobs

--- src/Kernel/Server/Job/Queue/QueueDispatcher.php
+++ src/Kernel/Server/Job/Queue/QueueDispatcher.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Nyxio\Kernel\Server\Job\Queue;
 
+use Nyxio\Contract\Config\ConfigInterface;
 use Nyxio\Contract\Kernel\Server\Job\DispatcherInterface;
 use Nyxio\Contract\Kernel\Server\Job\Queue\QueueDispatcherInterface;
 use Nyxio\Contract\Kernel\Server\Job\Queue\QueueInterface;
+use Swoole\Http\Server;
 
 class QueueDispatcher implements QueueDispatcherInterface
 {
+    private const DEFAULT_DELAY = 1000; // ms
+
     public function __construct(
+        private readonly Server $server,
+        private readonly ConfigInterface $config,
         private readonly QueueInterface $queue,
         private readonly DispatcherInterface $dispatcher
     ) {
@@ -18,8 +24,10 @@ class QueueDispatcher implements QueueDispatcherInterface
 
     public function launch(): void
     {
-        foreach ($this->queue->getQueue() as $taskData) {
-            $this->dispatcher->dispatch($taskData);
-        }
+        $this->server->tick($this->config->get('app.queue.delay', self::DEFAULT_DELAY), function (): void {
+            foreach ($this->queue->getQueue() as $taskData) {
+                $this->dispatcher->dispatch($taskData);
+            }
+        });
     }
 }
